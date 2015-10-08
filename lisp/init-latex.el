d9e949c7fff9edd3325889d02ccb5f1a52f10b24
Christos Gkantsidis <chrisgk@microsoft.com>
2015-10-08 12:20:09 +0100

    added reftex in latex

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -1,58 +1,63 @@
-; Initialization of LaTeX-math-mode
-; Instructions from:
-; 1. http://www.emacswiki.org/emacs/AUCTeX
-; 2. https://github.com/xyguo/emacs.d/blob/master/lisp/init-auctex.el
+;;; package --- Summary
+;; Initialization of LaTeX-math-mode
+;; Instructions from:
+;; 1. http://www.emacswiki.org/emacs/AUCTeX
+;; 2. https://github.com/xyguo/emacs.d/blob/master/lisp/init-auctex.el
 
+;;; Commentary:
+;; Includes various packages that make working with Emacs easy.
+
+;;; Code:
 (require-package 'auctex)
 
 (require 'tex-mik)
 
-(mapc (lambda (mode) 
-      (add-hook 'LaTeX-mode-hook mode)) 
-      (list ;; 'auto-fill-mode 
-            'LaTeX-math-mode 
-            'turn-on-reftex 
-             'TeX-fold-mode 
-             'linum-mode 
-             'auto-complete-mode 
-             'autopair-mode 
-             'outline-minor-mode)) 
+(mapc (lambda (mode)
+        (add-hook 'LaTeX-mode-hook mode))
+      (list ;; 'auto-fill-mode
+       'LaTeX-math-mode
+       'turn-on-reftex
+       'TeX-fold-mode
+       'linum-mode
+       'auto-complete-mode
+       'autopair-mode
+       'outline-minor-mode))
 
 
-(add-hook 'LaTeX-mode-hook 
-          (lambda () 
-           (setq ;;TeX-auto-untabify t     ; remove all tabs before saving 
-                  TeX-engine 'xetex       ; use xelatex default 
-                  TeX-show-compilation nil) ; display compilation windows 
-            (TeX-global-PDF-mode t)       ; PDF mode enable, not plain 
-            (setq TeX-save-query nil) 
-            (imenu-add-menubar-index) 
-            ;;(define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol) 
-            )) 
-
-             
-;; configuration for TeX-fold-mode 
-;; add entries you want to be fold, or comment that needn't to be fold. 
-(setq TeX-fold-env-spec-list 
-      (quote (("[figure]" ("figure")) 
-              ("[table]" ("table")) 
-              ("[itemize]" ("itemize")) 
-              ("[description]" ("description")) 
-              ("[tabular]" ("tabular")) 
-              ("[frame]" ("frame")) 
-              ("[array]" ("array")) 
-              ("[code]" ("lstlisting")) 
-;;              ("[eqnarray]" ("eqnarray")) 
-              ))) 
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (setq ;;TeX-auto-untabify t     ; remove all tabs before saving
+             TeX-engine 'xetex       ; use xelatex default
+             TeX-show-compilation nil) ; display compilation windows
+            (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
+            (setq TeX-save-query nil)
+            (imenu-add-menubar-index)
+            ;;(define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)
+            ))
 
 
-;; configuration for reftex 
- 
- 
-;; make the toc displayed on the left 
-(setq reftex-toc-split-windows-horizontally t) 
-;; adjust the fraction 
-(setq reftex-toc-split-windows-fraction 0.3) 
+;; configuration for TeX-fold-mode
+;; add entries you want to be fold, or comment that needn't to be fold.
+(setq TeX-fold-env-spec-list
+      (quote (("[figure]" ("figure"))
+              ("[table]" ("table"))
+              ("[itemize]" ("itemize"))
+              ("[description]" ("description"))
+              ("[tabular]" ("tabular"))
+              ("[frame]" ("frame"))
+              ("[array]" ("array"))
+              ("[code]" ("lstlisting"))
+              ;;              ("[eqnarray]" ("eqnarray"))
+              )))
+
+
+;; configuration for reftex
+
+
+;; make the toc displayed on the left
+(setq reftex-toc-split-windows-horizontally t)
+;; adjust the fraction
+(setq reftex-toc-split-windows-fraction 0.3)
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
@@ -66,25 +71,25 @@
 (setq TeX-PDF-mode t)
 
 (defun latex-help-get-cmd-alist () ;corrected version:
-"Scoop up the commands in the index of the latex info manual.
+  "Scoop up the commands in the index of the latex info manual.
 The values are saved in `latex-help-cmd-alist' for speed."
-    ;; mm, does it contain any cached entries
-    (if (not (assoc "\\begin" latex-help-cmd-alist))
-        (save-window-excursion
-    (setq latex-help-cmd-alist nil)
-    (Info-goto-node (concat latex-help-file "Command Index"))
-          (end-of-buffer)
-          (while (re-search-backward "^\\* \\(.+\\): *\\(.+\\)\\." nil t)
-            (setq key (ltxh-buffer-substring (match-beginning 1) (match-end 1)))
-            (setq value (ltxh-buffer-substring (match-beginning 2) (match-end 2)))
-            (setq latex-help-cmd-alist
-                  (cons (cons key value) latex-help-cmd-alist))))
-      )
-    latex-help-cmd-alist
+  ;; mm, does it contain any cached entries
+  (if (not (assoc "\\begin" latex-help-cmd-alist))
+      (save-window-excursion
+        (setq latex-help-cmd-alist nil)
+        (Info-goto-node (concat latex-help-file "Command Index"))
+        (end-of-buffer)
+        (while (re-search-backward "^\\* \\(.+\\): *\\(.+\\)\\." nil t)
+          (setq key (ltxh-buffer-substring (match-beginning 1) (match-end 1)))
+          (setq value (ltxh-buffer-substring (match-beginning 2) (match-end 2)))
+          (setq latex-help-cmd-alist
+                (cons (cons key value) latex-help-cmd-alist))))
     )
-       
+  latex-help-cmd-alist
+  )
 
-(require-package 'cdlatex)       
+
+(require-package 'cdlatex)
 (require 'cdlatex)
 
 ;;
@@ -92,20 +97,20 @@ The values are saved in `latex-help-cmd-alist' for speed."
 ;;
 (require-package 'outline-magic)
 (add-hook 'latex-mode-hook      ; or 'LaTeX-mode-hook for AUCTeX
-    (lambda ()
-        (setq outline-promotion-headings
-            '("\\chapter" "\\section" "\\subsection"
-            "\\subsubsection" "\\paragraph" "\\subparagraph"))))
+          (lambda ()
+            (setq outline-promotion-headings
+                  '("\\chapter" "\\section" "\\subsection"
+                    "\\subsubsection" "\\paragraph" "\\subparagraph"))))
 
 (add-hook 'texinfo-mode-hook
-    (lambda ()
-        (setq outline-promotion-headings
-            '("@chapter" "@section" "@subsection" "@subsubsection" nil
-              "@unnumbered" "@unnumberedsec" "@unnumberedsubsec"
-                                  "@unnumberedsubsubsec" nil
-              "@appendix" "@appendixsec" "@appendixsubsec"
-                                    "@appendixsubsubsec" nil
-              "@chapheading" "@heading" "@subheading" "@subsubheading")))) 
+          (lambda ()
+            (setq outline-promotion-headings
+                  '("@chapter" "@section" "@subsection" "@subsubsection" nil
+                    "@unnumbered" "@unnumberedsec" "@unnumberedsubsec"
+                    "@unnumberedsubsubsec" nil
+                    "@appendix" "@appendixsec" "@appendixsubsec"
+                    "@appendixsubsubsec" nil
+                    "@chapheading" "@heading" "@subheading" "@subsubheading"))))
 
 ;;
 ;; Emacs minor mode for avoiding cliches and bad grammar when writing about art (or other topics)
@@ -128,6 +133,11 @@ The values are saved in `latex-help-cmd-alist' for speed."
 
 (setq doc-view-ghostscript-program "gswin64c")
 
+;; Turn on RefTeX for AUCTeX, http://www.gnu.org/s/auctex/manual/reftex/reftex_5.html
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+;; Make RefTeX interact with AUCTeX, http://www.gnu.org/s/auctex/manual/reftex/AUCTeX_002dRefTeX-Interface.html
+(setq reftex-plug-into-AUCTeX t)
+
 ;;
 ;; EXPERIMENTAL AND NOT WORKING
 ;;
@@ -140,5 +150,6 @@ The values are saved in `latex-help-cmd-alist' for speed."
 ;;
 ;; End of configuration
 ;;
-      
+
 (provide 'init-latex)
+;;; init-latex.el ends here
