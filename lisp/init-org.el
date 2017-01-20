09ed84598b044e1a5dcf9806e1fd4e6ff3a02bf1
Christos Gkantsidis <gkantsidis@gmail.com>
2017-01-20 10:30:59 +0000

    added function to search directory for list of org files

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -370,10 +370,39 @@ typical word processor."
      (sqlite . t))))
 
 
+
+;;; Helper to collect org files
+
+;; ## Search org files recursively and add it to agenda
+;; https://stackoverflow.com/questions/11384516/how-to-make-all-org-files-under-a-folder-added-in-agenda-list-automatically
+;; recursively find .org files in provided directory
+;; modified from an Emacs Lisp Intro example
+(defun sa-find-org-file-recursively (&optional directory filext)
+  "Return .org and .org_archive files recursively from DIRECTORY.
+If FILEXT is provided, return files with extension FILEXT instead."
+  (interactive "DDirectory: ")
+  (let* (org-file-list
+         (case-fold-search t)         ; filesystems are case sensitive
+         (file-name-regex "^[^.#].*") ; exclude dot, autosave, and backup files
+         (filext (or filext "org$\\\|org_archive"))
+         (fileregex (format "%s\\.\\(%s$\\)" file-name-regex filext))
+         (cur-dir-list (directory-files directory t file-name-regex)))
+    ;; loop over directory listing
+    (dolist (file-or-dir cur-dir-list org-file-list) ; returns org-file-list
+      (cond
+       ((file-regular-p file-or-dir) ; regular files
+        (if (string-match fileregex file-or-dir) ; org files
+            (add-to-list 'org-file-list file-or-dir)))
+       ((file-directory-p file-or-dir)
+        (dolist (org-file (sa-find-org-file-recursively file-or-dir filext)
+                          org-file-list) ; add files found to result
+          (add-to-list 'org-file-list org-file)))))))
+
+
 (provide 'init-org)
 
 
-;;; In case the user has further customizations, call them
+;;; In case there are local machine-specific customizations, call them
 
 (when
     (file-exists-p "init-org-mylocal.el")
