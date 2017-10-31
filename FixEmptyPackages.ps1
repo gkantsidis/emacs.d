d66d891fd5cd25a703c02e59cdc52566b486973f
Christos Gkantsidis <chrisgk@microsoft.com>
2017-10-31 17:27:41 +0200

    FIX: script checks for elpa directories

--- FixEmptyPackages.ps1
+++ FixEmptyPackages.ps1
@@ -3,7 +3,17 @@ param(
     [switch]$RemoveEmpty
 )
 
-[object[]]$empty = Get-ChildItem .\elpa-25.1 -Include *.el -Recurse | Where-Object -Property Length -eq 0
+[System.IO.DirectoryInfo[]]$elpa = Get-ChildItem -Path . -Filter elpa-* -Directory
+if (($elpa -eq $null) -or ($elpa.Length -eq 0)) {
+    Write-Error -Message "Cannot determine elpa directory"
+    return
+}
+if ($elpa.Length -ge 2) {
+    Write-Error -Message "Multiple elpa directories detected"
+    return
+}
+
+[object[]]$empty = Get-ChildItem $elpa[0] -Include *.el -Recurse | Where-Object -Property Length -eq 0
 
 foreach ($faulty in $empty) {
     Write-Warning -Message "File '$faulty' appears to be empty"
